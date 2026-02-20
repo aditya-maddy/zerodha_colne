c2a6fe6c67c85ab92a23f87a978e8e4d4b84bb99
aditya mahadik <adityamahadik6530@gmail.com>
2026-02-20 18:28:45 +0530

    dashboard complete

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // <-- Add this
 });
-
-// Add passport-local-mongoose plugin
-userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
